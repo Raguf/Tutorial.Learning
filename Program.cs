e109fd331a6379f0891730f7dd145e61e6e81562
Raguf <ragufym@code.edu.az>
2022-02-23 16:04:02 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            Console.WriteLine($"Write first number: ");
+            Console.ReadLine();
+
+            Console.WriteLine($"Write second number: ");
+            Console.ReadLine();
+
+
+            Console.WriteLine($"Sum of 2 number: ");
+        }
+    }
+}
